d7d9fcbbefe9787059b925b4e7c4d7b2f8562e67
shyntaayu <shynta.add@gmail.com>
2020-06-01 06:29:17 +0700

    + toggle

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule, MatButtonToggle } from '@angular/material/button-toggle';
 
 const MaterialComponents = [
   MatButtonModule,
-  MatIconModule
+  MatIconModule,
+  MatButtonToggleModule
 ]
 
 @NgModule({
